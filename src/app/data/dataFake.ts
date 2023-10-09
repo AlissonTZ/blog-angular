7374328acdfadbcb421190772704e611035db5d4
AlissonTZ <Alissontuze@gmail.com>
2023-10-09 19:46:25 -0300

    Projeto Angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,51 @@
+export const dataFake = [
+{
+    "id":"1",
+    "title": "Nova Versão do Angular",
+    "description": `
+    O Angular 17, a mais recente versão do popular framework da Web baseado em TypeScript do Google, foi lançado em 20 de julho de 2023. A nova versão traz uma série de melhorias e novos recursos, incluindo:
+
+    Melhorias no desempenho: O Angular 17 foi otimizado para melhorar o desempenho de aplicativos Angular, com melhorias em áreas como a renderização de componentes, o carregamento de módulos e a injeção de dependências.
+
+    Novos recursos: O Angular 17 introduz uma série de novos recursos, incluindo suporte para o TypeScript 4.7, o novo componente MatTable do Material Design e o novo pacote @angular/flex-layout.
+
+    A nova versão do Angular está disponível para download no site oficial do framework.
+  `,
+    "photoCover": "https://miro.medium.com/v2/resize:fit:1400/1*I9kHMrHLYEryMFQbrdPhkQ.png"
+},
+{
+    "id":"2",
+    "title": "Setor de Tecnologia cresce nesse inicio de 2023",
+    "description": `O setor de tecnologia segue em crescimento no início de 2023, impulsionado por uma série de fatores, incluindo a digitalização das empresas, o aumento da demanda por serviços de computação em nuvem e a crescente popularidade da inteligência artificial.
+
+    De acordo com um levantamento da consultoria IDC, os gastos mundiais com TI devem totalizar US$ 4,5 trilhões em 2023, um crescimento de 2,4% em relação ao ano anterior. No Brasil, o mercado de TI deve crescer 6,2%, chegando a valer US$ 80 bilhões.`,
+    "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2022/06/Tecnologia.jpg"
+},
+{
+    "id":"3",
+    "title": "Presença feminina em cargos de tecnologia aumenta",
+    "description": `A presença feminina em cargos de tecnologia vem aumentando nos últimos anos, embora ainda seja um número inferior ao de homens. De acordo com dados do Cadastro Geral de Empregados e Desempregados (CAGED), em 2022, as mulheres representavam 23% dos trabalhadores do setor de tecnologia no Brasil.
+
+    Esse aumento pode ser atribuído a uma série de fatores, incluindo a maior conscientização sobre a importância da diversidade nas empresas, o aumento da oferta de cursos e treinamentos voltados para mulheres e a criação de programas e iniciativas para incentivar a participação feminina na tecnologia.`,
+    "photoCover": "https://s24534.pcdn.co/carreira-sucesso/wp-content/uploads/sites/3/2022/03/presenca-feminia-tecnologia.jpg"
+},
+{
+    "id":"4",
+    "title": "Oito tendências de tecnologia para 2023",
+    "description": `
+    O ano de 2023 promete ser um ano de grandes avanços tecnológicos. A seguir, apresentamos oito tendências que devem marcar este período:
+    
+    Inteligência artificial (IA): A IA continuará a se tornar cada vez mais sofisticada, sendo usada em uma ampla gama de aplicações, desde a automação de tarefas até a tomada de decisões.
+    Realidade virtual (VR) e realidade aumentada (AR): A VR e a AR estão se tornando mais acessíveis e envolventes, com o potencial de revolucionar a forma como interagimos com o mundo digital.
+    Internet das coisas (IoT): A IoT continuará a se expandir, com um número cada vez maior de dispositivos conectados à internet.
+    Computação em nuvem: A computação em nuvem continuará a ser uma das principais tendências tecnológicas, oferecendo flexibilidade, escalabilidade e economia de custos.
+    Cibersegurança: A cibersegurança será uma preocupação crescente, à medida que os ataques cibernéticos se tornam cada vez mais sofisticados.
+    Blockchain: O blockchain continuará a ser uma tecnologia emergente com o potencial de revolucionar a forma como armazenamos e compartilhamos dados.
+    Tecnologias sustentáveis: As tecnologias sustentáveis, como a energia renovável e a eficiência energética, ganharão força à medida que as empresas e os indivíduos buscam formas de reduzir seu impacto ambiental.
+    Web3: A Web3, ou a próxima geração da internet, está se desenvolvendo com o potencial de descentralizar a internet e dar mais controle aos usuários.
+    Essas tendências irão moldar o futuro da tecnologia e terão um impacto significativo em nossas vidas.`,
+    
+    "photoCover": "https://ravel.com.br/blog/wp-content/uploads/2023/02/Tendencias-de-tecnologia-2023-Capa.jpg"
+},
+
+]
